f6140be346535b8cb3f1e173ca614ebed0e4f849
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-07-24 17:32:27 +0900

    feat: email auth api 관련 Swagger 설정 추가

--- src/main/java/com/canvi/hama/domain/auth/controller/EmailAuthController.java
+++ src/main/java/com/canvi/hama/domain/auth/controller/EmailAuthController.java
@@ -5,6 +5,9 @@ import com.canvi.hama.common.response.BaseResponse;
 import com.canvi.hama.common.response.BaseResponseStatus;
 import com.canvi.hama.domain.auth.service.AuthService;
 import com.canvi.hama.domain.auth.service.EmailAuthService;
+import com.canvi.hama.domain.auth.swagger.SendAuthCodeApi;
+import com.canvi.hama.domain.auth.swagger.VerifyAuthCodeApi;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Email Auth")
 @RestController
 @RequestMapping("/api/email-auth")
 @RequiredArgsConstructor
@@ -20,6 +24,7 @@ public class EmailAuthController {
     private final EmailAuthService emailAuthService;
     private final AuthService authService;
 
+    @SendAuthCodeApi
     @PostMapping("/send-code")
     public ResponseEntity<BaseResponse<Void>> sendAuthCode(@RequestParam String email) {
         if (!authService.isEmailAvailable(email)) {
@@ -29,6 +34,7 @@ public class EmailAuthController {
         return ResponseEntity.ok(new BaseResponse<>(BaseResponseStatus.SUCCESS));
     }
 
+    @VerifyAuthCodeApi
     @PostMapping("/verify-code")
     public ResponseEntity<BaseResponse<Void>> verifyAuthCode(@RequestParam String email, @RequestParam String code) {
         if (emailAuthService.verifyAuthCode(email, code)) {
